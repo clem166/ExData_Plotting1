dcab1eeae91426722614cea286ee6868bac1c516
clem166 <42425647+clem166@users.noreply.github.com>
2019-03-25 19:17:56 +1100

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -15,7 +15,7 @@ data2$DateTime = ymd_hms(paste0(data2$Date, data2$Time))
 
 
 #Plot1
-hist(data2$Global_active_power, xlab = "Global Active Power (kilowatts)", ylab = "Frequency", col="red")
+hist(data2$Global_active_power, xlab = "Global Active Power (kilowatts)", ylab = "Frequency", col="red", main="Global Active Power")
 
 dev.copy(png, file="plot1.png", width=480, height=480)
 dev.off()    
